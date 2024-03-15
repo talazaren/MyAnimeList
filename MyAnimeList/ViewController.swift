14c5a2dd0279e704d3e735146ff1fc6a8bc56cc9
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-03-15 18:21:29 +0300

    Initial Commit

---
+++ MyAnimeList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnimeList
+//
+//  Created by Tatiana Lazarenko on 3/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
